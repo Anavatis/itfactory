a81666f173f2241a4b56eef745f98e1a14e71509
Anavatis <44580637+Anavatis@users.noreply.github.com>
2022-03-15 14:42:58 +0300

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from flask import Flask
+
+from .database import db
+load_dotenv()
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    register_extensions(app)
+    register_blueprints(app)
+
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+
+
+def register_blueprints(app):
+    ...
